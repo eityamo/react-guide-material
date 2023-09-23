042a840c3a66e70f1a978ac4ad47176b9463fbfe
eityamo <eityamo@gmail.com>
2023-09-23 12:38:57 +0900

    add:スプレッド演算子

--- 03_js_basic/060_spread_rest/start/main.js
+++ 03_js_basic/060_spread_rest/start/main.js
@@ -1,25 +1,36 @@
 const nums = [3, 1, 4, 1, 5, 10, 2, 6];
 
-const result = Math.max(3, 1, 4, 1, 5, 10, 2, 6);
+const result = Math.max(...nums);
 console.log(result);
 
-
+// 元ある配列から新しい配列を作成
 let arr1 = [1, 2, 3];
 let arr2 = [4, 5, 6];
-let newArr = [...arr1];
+let newArr = [...arr1, 10, ...arr2];
+let newArr1 = arr1;
+newArr1.push(4);
+console.log(newArr);
+console.log(newArr1);
+console.log(arr1);
 
-console.log(newArr2);
+// 異なる配列
+console.log(newArr === arr1);
+// 同じ配列
+console.log(newArr1 === arr1);
 
 const obj = {
   name: "Tom",
   age: 22,
 };
 const newObj = { ...obj };
+newObj.name = "John"
 
-console.log(newObj);
+console.log(newObj, obj);
 
+// 引数が可変の際に有効
 const restA = (...argA) => console.log(argA);
 restA(1, 3, 4)
 
+// 特定の引数だけ、変数を分けたい場合
 const restB = (n, ...argB) => console.log(argB);
 restB(1, 3, 4)
