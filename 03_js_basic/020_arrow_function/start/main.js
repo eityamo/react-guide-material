fc238537faa9b52c3a562566169f365cdded13d7
eityamo <eityamo@gmail.com>
2023-09-23 11:29:04 +0900

    add:アロー関数

--- 03_js_basic/020_arrow_function/start/main.js
+++ 03_js_basic/020_arrow_function/start/main.js
@@ -1,3 +1,18 @@
 function fn(number) {
   return number * 2;
 }
+
+// アロー関数の記述を少なくする
+
+console.log(fn(2));
+
+const fnArrow = (number) => {
+  console.log(number);
+  return number * 2;
+};
+
+const fnArrowObj = (number) => ({
+  result: number * 2,
+});
+
+console.log(fnArrowObj(2));
