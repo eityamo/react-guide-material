ccd95c3e015acec54adcda48bded31bf41a5c00a
eityamo <eityamo@gmail.com>
2023-09-26 09:44:44 +0900

    add:propsの使い方

--- 04_react_basic/src/090_props/start/Example.js
+++ 04_react_basic/src/090_props/start/Example.js
@@ -1,5 +1,11 @@
 import Child from "./components/Child";
 
-const Example = () => <Child />;
+const Example = () => (
+  <>
+    <Child />
+    <Child color="" />
+    <Child color="red" />
+  </>
+);
 
 export default Example;
