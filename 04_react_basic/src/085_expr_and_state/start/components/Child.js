8885ca5d44606db9652c37561ce070f49f2398bc
eityamo <eityamo@gmail.com>
2023-09-26 09:32:23 +0900

    add:式と文の違い

--- 04_react_basic/src/085_expr_and_state/start/components/Child.js
+++ 04_react_basic/src/085_expr_and_state/start/components/Child.js
@@ -6,9 +6,13 @@
 import "./Child.css";
 
 const Child = () => {
+  const a = 1;
+  console.log(1 === 1);
+  
   return (
     <div className="component">
       <h3>式と文</h3>
+      {true ? "hello" : "bye"}
     </div>
   );
 };
