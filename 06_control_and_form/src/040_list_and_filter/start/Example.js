ec1f592d8a6d3375f38f8495baf437c581fc53c3
eityamo <eityamo@gmail.com>
2023-10-09 10:48:02 +0900

    add:フィルターメソッドの使い方

--- 06_control_and_form/src/040_list_and_filter/start/Example.js
+++ 06_control_and_form/src/040_list_and_filter/start/Example.js
@@ -3,13 +3,19 @@ import { useState } from "react";
 const animals = ["Dog", "Cat", "Rat"];
 
 const Example = () => {
+  const [filterVal, setFliterVal] = useState("");
+
   return (
     <>
       <h3>配列のフィルター</h3>
+      <input
+        type="text"
+        value={filterVal}
+        onChange={(e) => setFliterVal(e.target.value)}
+      />
       <ul>
-        {animals
-          .map((animal) => (
-          <li>{animal}</li>
+        {animals.filter(animal => animal.indexOf(filterVal) !== -1).map((animal) => (
+          <li key={animal}>{animal}</li>
         ))}
       </ul>
     </>
