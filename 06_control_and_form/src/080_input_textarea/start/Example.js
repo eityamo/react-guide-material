bcd29c42a6d428ec50dd605e44f0f1213b58c64a
eityamo <eityamo@gmail.com>
2023-10-09 23:08:13 +0900

    add:inputとtextarea

--- 06_control_and_form/src/080_input_textarea/start/Example.js
+++ 06_control_and_form/src/080_input_textarea/start/Example.js
@@ -1,10 +1,23 @@
+import { useState } from "react";
+
 const Example = () => {
+  const [val, setVal] = useState();
+  const clearVal = () => setVal();
   return (
-    <p style={{ textAlign: "center" }}>
-      startフォルダの内容が表示されます。
-      <br />
-      練習用に使ってください！
-    </p>
+    <>
+      <p style={{ textAlign: "center" }}>
+        startフォルダの内容が表示されます。
+        <br />
+        練習用に使ってください！
+      </p>
+      <label htmlFor="456">label</label>
+      <div>
+        <input id="123" placeholder="hello" value={val} onChange={(e) => setVal(e.target.value)} />
+        <textarea id="456" placeholder="hello" value={val} onChange={(e) => setVal(e.target.value)} />
+      </div>
+      <h3>{val}</h3>
+      <button onClick={clearVal}>クリア</button>
+    </>
   );
 };
 
