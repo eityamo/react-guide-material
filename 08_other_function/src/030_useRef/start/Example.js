82a597bf2dc05ff1bb3254fd0594b41912b5af23
eityamo <eityamo@gmail.com>
2023-11-14 22:21:34 +0900

    add: refで動画をプレイ

--- 08_other_function/src/030_useRef/start/Example.js
+++ 08_other_function/src/030_useRef/start/Example.js
@@ -8,9 +8,38 @@ const Case1 = () => {
   return (
     <div>
       <h3>ユースケース1</h3>
-      <input type="text" ref={inputRef} value={value} onChange={(e) => setValue(e.target.value)} />
+      <input
+        type="text"
+        ref={inputRef}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
       <button onClick={() => inputRef.current.focus()}>
         インプット要素をフォーカスする
+      </button>
+    </div>
+  );
+};
+
+const Case2 = () => {
+  const [playing, setPlaying] = useState(false);
+  const videoRef = useRef();
+  return (
+    <div>
+      <video style={{ maxWidth: "100%" }} ref={videoRef}>
+        <source src="./sample.mp4"></source>
+      </video>
+      <button
+        onClick={() => {
+          if (playing) {
+            videoRef.current.pause();
+          } else {
+            videoRef.current.play();
+          }
+          setPlaying((prev) => !prev);
+        }}
+      >
+        {playing ? "STOP" : "PLAY"}
       </button>
     </div>
   );
@@ -20,6 +49,7 @@ const Example = () => {
   return (
     <>
       <Case1 />
+      <Case2 />
     </>
   );
 };
