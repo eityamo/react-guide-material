6b707b2193462c232067280c671c1bcb8d3bb357
eityamo <eityamo@gmail.com>
2023-11-18 12:14:15 +0900

    add:純粋関数

--- 10_functional_programming/src/020_pure_function/start/Example.js
+++ 10_functional_programming/src/020_pure_function/start/Example.js
@@ -1,8 +1,12 @@
 const Example = () => {
+  const val1 = 1;
+  const val2 = 2;
+  const add = (val1) => {
+    return val1 + val2;
+  }
   return (
     <>
-      <h3>純粋関数</h3>
-      <p>fn(決まった引数) には 決まった戻り値 を返す</p>
+      <div>純粋関数:{add(val1)}</div>
     </>
   );
 };
