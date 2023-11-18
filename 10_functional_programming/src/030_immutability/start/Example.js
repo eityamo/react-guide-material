47b0fa26dc8f8b0e9288edbbb3d99ae3c70e6f84
eityamo <eityamo@gmail.com>
2023-11-18 20:57:30 +0900

    add:不変性

--- 10_functional_programming/src/030_immutability/start/Example.js
+++ 10_functional_programming/src/030_immutability/start/Example.js
@@ -1,4 +1,14 @@
 const Example = () => {
+
+  const num = { val: 2 };
+  const double = (num) => {
+    const newNum = { ...num };
+    newNum.val = num.val * 2;
+    return newNum;
+  }
+  const newNum = double(num);
+  console.log('newNum', newNum, 'num', num);
+  console.log(newNum === num);
   return (
     <>
       <h3>不変性（Immutability）</h3>
