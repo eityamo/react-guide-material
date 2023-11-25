02271883f66a9d8373d73288687bc4f60dc616a2
eityamo <eityamo@gmail.com>
2023-11-25 12:04:31 +0900

    add:Redux Toolkitにおけるミュータブルな値の変更

--- 13_redux/start/src/040_immer/components/CounterButton.js
+++ 13_redux/start/src/040_immer/components/CounterButton.js
@@ -4,10 +4,10 @@ import { add, minus } from "../store/modules/counter"
 const CounterButton = ({calcType, step}) => {
     
     const dispatch = useDispatch();
-    console.log(add(step))
+    // console.log(add(step))
     const clickHandler = () => {
         const action = calcType === '+' ? add(step) : minus(step);
-        console.log(action)
+        // console.log(action)
         dispatch(action);
     }
 
