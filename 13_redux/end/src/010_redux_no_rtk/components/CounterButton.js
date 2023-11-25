ee6a36df99e6722c6e87871e7bcedce52fff7ff4
eityamo <eityamo@gmail.com>
2023-11-25 10:11:16 +0900

    add:redux

--- 13_redux/end/src/010_redux_no_rtk/components/CounterButton.js
+++ 13_redux/end/src/010_redux_no_rtk/components/CounterButton.js
@@ -1,5 +1,5 @@
 import { useDispatch } from "react-redux";
-import { useCounterDispatch } from "../context/CounterContext";
+// import { useCounterDispatch } from "../context/CounterContext";
 
 const CounterButton = ({calcType, step}) => {
     
