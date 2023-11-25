399c72278e01cf8e80cddb83e441e583c4a8e049
eityamo <eityamo@gmail.com>
2023-11-25 10:35:36 +0900

    add:複数のreducerを使う

--- 13_redux/start/src/015_multiple_reducers/components/CounterButton.js
+++ 13_redux/start/src/015_multiple_reducers/components/CounterButton.js
@@ -1,13 +1,11 @@
 import { useDispatch } from "react-redux";
-import { useCounterDispatch } from "../context/CounterContext";
 
 const CounterButton = ({calcType, step}) => {
     
     const dispatch = useDispatch();
-    // const dispatch = useCounterDispatch();
     
     const clickHandler = () => {
-        dispatch({ type: calcType, step });
+        dispatch({ type: 'counter/' + calcType, step });
     }
 
     return <button onClick={clickHandler}>{calcType}{step}</button>
