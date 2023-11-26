8e812d6889a70c66f355444d62a541afeb79de9a
eityamo <eityamo@gmail.com>
2023-11-26 09:17:34 +0900

    add:非同期処理のステータスを画面に表示

--- 13_redux/start/src/060_createAsyncThunk/store/modules/counter.js
+++ 13_redux/start/src/060_createAsyncThunk/store/modules/counter.js
@@ -1,11 +1,11 @@
-import { createSlice } from "@reduxjs/toolkit";
-
-import { asyncCount } from "../../api/counter"
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { asyncCount } from "../../api/counter";
 
 const counter = createSlice({
-  name: 'counter',
+  name: "counter",
   initialState: {
-    count: 0
+    count: 0,
+    status: "",
   },
   reducers: {
     add(state, { type, payload }) {
@@ -19,11 +19,31 @@ const counter = createSlice({
       // const newState = { ...state };
       // newState.count = state.count - payload
       // return newState;
-    }
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(addAsyncWithStatus.pending, (state) => {
+      state.status = 'loading...'
+    })
+    .addCase(addAsyncWithStatus.fulfilled, (state, action) => {
+      state.status = '取得済'
+      state.count = state.count + action.payload;
+    })
+    .addCase(addAsyncWithStatus.rejected, (state) => {
+      state.status = 'エラー'
+    })
   }
 });
 
 const { add, minus } = counter.actions;
+
+const addAsyncWithStatus = createAsyncThunk(
+  "counter/asyncThunk",
+  async (payload) => {
+    const response = await asyncCount(payload);
+    return response.data;
+  }
+);
 
 const addAsync = (payload) => {
   return async (dispatch, getState) => {
@@ -31,9 +51,8 @@ const addAsync = (payload) => {
     console.log(state);
     const response = await asyncCount(payload);
     dispatch(add(response.data));
-  }
-}
+  };
+};
 
-
-export { add, minus, addAsync }
-export default counter.reducer
+export { add, minus, addAsync, addAsyncWithStatus };
+export default counter.reducer;
