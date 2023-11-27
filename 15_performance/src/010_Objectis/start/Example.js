2df6e88093e52c97bcdfb915d4d1a0d20804b1fe
eityamo <eityamo@gmail.com>
2023-11-27 23:20:19 +0900

    add:Stqteの比較処理

--- 15_performance/src/010_Objectis/start/Example.js
+++ 15_performance/src/010_Objectis/start/Example.js
@@ -1,10 +1,39 @@
+import { useState } from "react";
+import "../end/Example";
+
 const Example = () => {
+  console.log("render");
+  const [countA, setCountA] = useState({
+    val: 0,
+  });
+
+  const obj1 = { val: 0 };
+  const obj2 = obj1;
+  const obj3 = { val: 0 };
+  const isSame = Object.is(obj1, obj2);
+  const isSame2 = Object.is(obj1, obj3);
+  console.log(isSame);
+  console.log(isSame2);
+
   return (
-    <>
-    <h3>再レンダリングが発生する条件</h3>
-    <p>stateの値が変更された時</p>
-    <p>Object.isによって変更は検知される</p>
-    </>
+    <div className="parent">
+      <div>
+        <h3>再レンダリング？</h3>
+        <button
+          onClick={() => {
+            setCountA((prev) => {
+              const newState = { ...prev }
+              return newState;
+            });
+          }}
+        >
+          ボタンA
+        </button>
+      </div>
+      <div>
+        <p>ボタンAクリック回数：{countA.val}</p>
+      </div>
+    </div>
   );
 };
 
