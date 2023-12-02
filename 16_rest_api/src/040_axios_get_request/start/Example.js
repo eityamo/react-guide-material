d2b3584cfd120481a3460ba5840b2391675aa18d
eityamo <eityamo@gmail.com>
2023-12-02 12:59:18 +0900

    Axios

--- 16_rest_api/src/040_axios_get_request/start/Example.js
+++ 16_rest_api/src/040_axios_get_request/start/Example.js
@@ -1,5 +1,16 @@
+import { useEffect } from "react";
+import axios from "axios";
+// POINT axiosでGetリクエスト
+// https://axios-http.com/
+
 const Example = () => {
-  return <></>;
+  useEffect(() => {
+    const getUser = async () => {
+      const res = await axios.get('http://localhost:3003/user')
+      console.log(res.data)
+    }
+    getUser();
+  })
 };
 
 export default Example;
