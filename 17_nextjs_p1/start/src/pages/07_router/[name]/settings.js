28ce25f514293a98515ac6c724075d63197cd696
eityamo <eityamo@gmail.com>
2023-12-05 23:48:41 +0900

    add:router.push

--- 17_nextjs_p1/start/src/pages/07_router/[name]/settings.js
+++ 17_nextjs_p1/start/src/pages/07_router/[name]/settings.js
@@ -2,8 +2,15 @@ import { useRouter } from "next/router";
 
 export default function Setting({ query }) {
   const router = useRouter();
-  console.log(router);
-  return <h1>Routerから取得:{router.query.name}</h1>;
+  const clickHandler = () => {
+    router.push('/', '/dummy-url');
+  };
+  return (
+    <>
+      <h1>Routerから取得:{router.query.name}</h1>
+      <button onClick={clickHandler}>アクションによる画面遷移</button>
+    </>
+  );
 }
 
 export async function getServerSideProps({ query }) {
