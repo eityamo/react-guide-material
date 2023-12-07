d59611491239b9a1f43c5e6009ac9677b9e87ac5
eityamo <eityamo@gmail.com>
2023-12-07 23:57:20 +0900

    add:Linkを使って画面遷移を行う

--- 17_nextjs_p1/start/src/pages/index.js
+++ 17_nextjs_p1/start/src/pages/index.js
@@ -1,3 +1,13 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <h1>home</h1>
+  return (
+    <>
+      <h1>Home</h1>
+      <Link href={{ pathname: "/07_router", query: { key: "value" } }}>
+        <a>/07_router</a>
+      </Link>
+      <a href="/07_router">/07_router</a>
+    </>
+  );
 }
