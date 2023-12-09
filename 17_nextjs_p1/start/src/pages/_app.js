5c65109b3feac280ad7c0f52721740579aefbeda
eityamo <eityamo@gmail.com>
2023-12-09 11:12:32 +0900

    【_app.js】サイト全体に設定を加える方法

--- 17_nextjs_p1/start/src/pages/_app.js
+++ 17_nextjs_p1/start/src/pages/_app.js
@@ -1,12 +1,11 @@
 import "../styles/globals.css";
 import { AppProvider } from "../context/AppContext";
+import Layout1 from "../components/layout/layout1";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <AppProvider>
-      <Component {...pageProps} />
-    </AppProvider>
-  );
+  const getLayout =
+    Component.getLayout ?? ((page) => <Layout1>{page}</Layout1>);
+  return <AppProvider>{getLayout(<Component {...pageProps} />)}</AppProvider>;
 }
 
 export default MyApp;
