16d82af876e6ba2ab8beb9f531b5ea3c804cb3b2
eityamo <eityamo@gmail.com>
2023-12-19 23:57:30 +0900

    add:SSRの挙動確認

--- 18_nextjs_p2/start/src/pages/010_SSR/index.js
+++ 18_nextjs_p2/start/src/pages/010_SSR/index.js
@@ -2,6 +2,9 @@ import { useState } from "react";
 
 export default function SSR() {
     console.log('hello');
+    useEffect(() => {
+        window.localStorage.setItem('key', 'value')
+    }, [])
     const [state, setState] = useState('bye');
     const val = 0;
     return (<h3>{state}</h3>)
