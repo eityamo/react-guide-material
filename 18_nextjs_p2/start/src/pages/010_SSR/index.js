eb01e8227246f1e5666f400f951e14594f95c19b
eityamo <eityamo@gmail.com>
2023-12-23 17:26:04 +0900

    add:getServerSideProps

--- 18_nextjs_p2/start/src/pages/010_SSR/index.js
+++ 18_nextjs_p2/start/src/pages/010_SSR/index.js
@@ -1,11 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-export default function SSR() {
-    console.log('hello');
-    useEffect(() => {
-        window.localStorage.setItem('key', 'value')
-    }, [])
-    const [state, setState] = useState('bye');
-    const val = 0;
-    return (<h3>{state}</h3>)
+export default function SSR({ message }) {
+  console.log("hello");
+  console.log(message);
+  useEffect(() => {
+    window.localStorage.setItem("key", "value");
+    document.cokkie = 'val=0; path=/;'
+  }, []);
+  const [state, setState] = useState("bye");
+  const val = 0;
+  return <h3>{state}</h3>;
+}
+
+export async function getServerSideProps(context) {
+  const { cokkie } = context.req.headers;
+  console.log('cokkie', cokkie);
+  return {
+    // redirect: {
+    //   destination: "/",
+    //   permanent: true,
+    // },
+    props: { message: "From Srever Side Props" },
+  };
 }
