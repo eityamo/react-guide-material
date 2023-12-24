d07932f904323bb9fe96cc91a7a0bd54839d0a27
eityamo <eityamo@gmail.com>
2023-12-24 13:48:45 +0900

    add:SG準備

--- 18_nextjs_p2/start/src/pages/020_SG/index.js
+++ 18_nextjs_p2/start/src/pages/020_SG/index.js
@@ -1,13 +1,4 @@
 // POINT getStaticPropsでSGを実装
 export default function IndexPage({ message }) {
-  return <h3>SG:{message}</h3>;
-}
-
-export async function getStaticProps() {
-
-    // console.log('getStaticProps');
-
-    return {
-        props: { message: 'From Static Props' }
-    }
+  return <h3>SG</h3>;
 }
