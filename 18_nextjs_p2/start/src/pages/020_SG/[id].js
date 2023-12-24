4340eec1412ad22de3a49650818443d44859444d
eityamo <eityamo@gmail.com>
2023-12-24 22:56:45 +0900

    add:ISR

--- 18_nextjs_p2/start/src/pages/020_SG/[id].js
+++ 18_nextjs_p2/start/src/pages/020_SG/[id].js
@@ -1,11 +1,15 @@
 import { useRouter } from "next/router";
 
-export default function Page({ id }) {
+export default function Page({ id, date }) {
   const router = useRouter();
   if (router.isFallback) {
-    return <h3>Loading</h3>
+    return <h3>Loading</h3>;
   }
-  return <h3>このページは{id}です。</h3>;
+  return (
+    <h3>
+      このページは{id}です。{date}
+    </h3>
+  );
 }
 
 export async function getStaticPaths() {
@@ -22,9 +26,12 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
+  const date = new Date();
   return {
     props: {
       id: params.id,
+      date: date.toJSON(),
     },
+    revalidate: 5,
   };
 }
