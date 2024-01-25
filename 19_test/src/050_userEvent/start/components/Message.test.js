2d3f5b728302609fac9dfafb1a4b9d9b82a58ace
eityamo <eityamo@gmail.com>
2024-01-25 14:39:57 +0900

    初期表示の正常性確認

--- 19_test/src/050_userEvent/start/components/Message.test.js
+++ 19_test/src/050_userEvent/start/components/Message.test.js
@@ -1,5 +1,14 @@
-// import { render, screen } from "@testing-library/react";
-// import Message from "./Message";
-// import userEvent from "@testing-library/user-event";
+import { render, screen } from "@testing-library/react";
+import Message from "./Message";
+import userEvent from "@testing-library/user-event";
 
-test('ダミーテスト:削除して記載してください。', () => {});
+describe("Messageコンポーネントの動作確認", () => {
+  describe("初期表示の正常性確認", () => {
+    test("入力された文字が画面上に存在", () => {
+      render(<Message />);
+      const h2El = screen.getByRole("heading", { name: "入力された文字:" });
+      expect(h2El).toBeInTheDocument();
+    });
+  });
+});
+test("ダミーテスト:削除して記載してください。", () => {});
