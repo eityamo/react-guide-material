c43017cab4e2dea439d67a4ef65f044613009333
eityamo <eityamo@gmail.com>
2024-01-27 12:25:50 +0900

    非同期処理のテスト

--- 19_test/src/060_async_test/start/components/GetUserData.test.js
+++ 19_test/src/060_async_test/start/components/GetUserData.test.js
@@ -8,4 +8,14 @@ describe("GetUserDataコンポーネントの動作確認", () => {
     const H1El = screen.getByRole("heading", { name: "通信中です！" });
     expect(H1El).toBeInTheDocument();
   });
+
+  test("外部データ取得後", async () => {
+    render(<GetUserData url={ENDPOINT_URL} />);
+    const H2El = await screen.findByRole("heading", { name: "プロフィール" });
+    expect(H2El).toBeInTheDocument();
+
+    const itemEls = await screen.findAllByRole("listitem");
+    expect(itemEls[0].textContent).toBe("ID: 1");
+    expect(itemEls[1].textContent).toBe("Name: CodeMafia");
+  });
 });
