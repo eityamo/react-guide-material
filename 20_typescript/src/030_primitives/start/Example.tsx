bbddcf65cb8741c68784c71a5991b15d69044805
eityamo <eityamo@gmail.com>
2024-01-28 22:50:20 +0900

    add:プリミティブ型の型定義

--- 20_typescript/src/030_primitives/start/Example.tsx
+++ 20_typescript/src/030_primitives/start/Example.tsx
@@ -1,5 +1,24 @@
 const Example = () => {
-  // 練習用
+  let str: string = "Hello";
+  str = "Bye";
+  console.log(str);
+
+  let num: number = 102;
+  console.log(num);
+
+  let bignum: bigint = 103n;
+  console.log(bignum);
+
+  let bool: boolean = true;
+  console.log(bool);
+
+  let nuulish: null = null;
+  let undefinedValue: undefined = undefined;
+
+  // リテラル型
+  let trueVal: true = true;
+  let num123: 123 = 123;
+  let strHello: "hello" = "hello";
 };
 
 export default Example;
