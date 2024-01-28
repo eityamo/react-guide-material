a23f8d511416f1880ed554c2f60101a36bd9ccec
eityamo <eityamo@gmail.com>
2024-01-28 23:06:19 +0900

    ユニオン型

--- 20_typescript/src/040_union_types/start/Example.tsx
+++ 20_typescript/src/040_union_types/start/Example.tsx
@@ -1,5 +1,19 @@
 const Example = () => {
-  // 練習用
+  let strOrNum: string | number = "Hello";
+  strOrNum = 123;
+  console.log(strOrNum);
+
+  let strOrNumOrBool: string | number | boolean = "Hello";
+  strOrNumOrBool = true;
+  console.log(strOrNumOrBool);
+
+  type HelloOrNum = "Hello" | number;
+  const hello: HelloOrNum = "Hello";
+  console.log(hello);
+
+  type DayOfWeek = "Monday" | "Tuesday" | "Wednesday";
+  const day: DayOfWeek = "Monday";
+  console.log(day);
 };
 
 export default Example;
