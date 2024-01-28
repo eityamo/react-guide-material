67f5f75f7febd2d33984a9abe41b9e4441bfb5da
eityamo <eityamo@gmail.com>
2024-01-28 23:21:30 +0900

    オブジェクトの型定義

--- 20_typescript/src/050_objects/start/Example.tsx
+++ 20_typescript/src/050_objects/start/Example.tsx
@@ -1,5 +1,17 @@
 const Example = () => {
-  // 練習用
+  const array1: number[] = [1, 2, 3];
+  const array2: string[] = ["Hello", "Bye"];
+  const array3: Array<string> = ["Hello", "Bye"];
+  const array4: (number | string)[] = [1, "Bye"];
+  const array5: Array<number | string> = [1, "Bye"];
+
+  type Person = { name: string; age?: number };
+  const obj1: Person = {
+    name: "Taro",
+    age: 18,
+  };
+
+  const users: Person[] = [{ name: "Taro" }, { name: "Hanako", age: 17 }];
 };
 
 export default Example;
