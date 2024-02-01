3c92aaf133dd9ac4c0460fb6a869d1ea22525d0c
eityamo <eityamo@gmail.com>
2024-02-02 00:32:14 +0900

    戻り値の型定義

--- 20_typescript/src/080_function_type/start/Example.tsx
+++ 20_typescript/src/080_function_type/start/Example.tsx
@@ -3,13 +3,18 @@ const Example = () => {
     return x + y;
   }
 
-  const sum2 = (x: number, y: number) => x + y;
+  const sum2 = (x: number, y: number): number => x + y;
 
   const result1 = sum1(1);
   console.log(result1);
 
-  // const result2 = sum1(1, '2');
+  const result2 = sum2(10, 20);
+  console.log(result2);
 
+  type Sum = (x: number, y: number) => number;
+  const sum3: Sum = (x, y) => x + y;
+  const result3 = sum3(1, 2);
+  console.log(result3);
 };
 
 export default Example;
