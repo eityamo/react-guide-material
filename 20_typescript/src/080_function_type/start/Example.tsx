345a06615d6578873d922ab750114cee5a38ee1c
eityamo <eityamo@gmail.com>
2024-02-01 23:49:52 +0900

    関数の引数

--- 20_typescript/src/080_function_type/start/Example.tsx
+++ 20_typescript/src/080_function_type/start/Example.tsx
@@ -1,5 +1,15 @@
 const Example = () => {
-  // 練習用
+  function sum1(x: number, y: number = 1) {
+    return x + y;
+  }
+
+  const sum2 = (x: number, y: number) => x + y;
+
+  const result1 = sum1(1);
+  console.log(result1);
+
+  // const result2 = sum1(1, '2');
+
 };
 
 export default Example;
