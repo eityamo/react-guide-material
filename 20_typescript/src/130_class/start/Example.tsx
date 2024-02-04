b0060aac2cb7b8061947296ef7ea0c101ae23006
eityamo <eityamo@gmail.com>
2024-02-04 23:14:51 +0900

    classの型定義

--- 20_typescript/src/130_class/start/Example.tsx
+++ 20_typescript/src/130_class/start/Example.tsx
@@ -1,5 +1,23 @@
+class User {
+  public name: string = "Taro";
+  private age: number = 18;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = 24;
+  }
+}
+
 const Example = () => {
-  // 練習用
+  const user = { name: "Hanako", age: 21 };
+  const user1 = new User("Hanako", 22);
+  console.log(user1);
+  return (
+    <div>
+      <p>Name: {user.name}</p>
+      <p>Age: {user.age}</p>
+    </div>
+  );
 };
 
 export default Example;
